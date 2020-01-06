ef19ceb4eb14db8eb55a9093b00ddc4f3f1bd8f1
MINJIN KIM <58720096+sara-kim@users.noreply.github.com>
2020-01-06 16:02:09 +0900

    0105
    m_ice button 오류

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,5 +5,29 @@ $(document).ready(function(){
     $tag.on('click', function (){
         $(this).addClass('on');
     });
+    
+    //m_ice slide
+	var $otherMenu=$("#article .ot_menu");
+	var $btn=$otherMenu.find("a").first();
+	var $text=$otherMenu.find("span");
+	var tgHref;
+	$btn.on("click",function  (e) {
+		e.preventDefault();
+		$(this).next().stop().slideDown();
+		$(this).next().on("click",function  () {
+			$(this).stop().slideUp();
+		});
+		$otherMenu.find(">ul>li>a").on("click",function  (e) {
+			e.preventDefault();
+			var tgTxt=$(this).text();
+			tgHref=$(this).attr("href");
+			$btn.find($text).text(tgTxt).closest("a").next().stop().slideUp();
+		});
+	});
 
+	//cashbox
+	var $button = $('#article > div.menu_wrap > ul > li:nth-child(2) > div > div.f_r_txt > button');
+	$button.on('click', function (){
+		alert();
+	});
 });
